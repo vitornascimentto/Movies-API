e34d8565693e8426bc8c38c6bf626639a8fb1bed
vitornascimentto <vitornascimentto0@gmail.com>
2021-04-12 22:22:49 -0300

    add favorite_movies table in the database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_000618) do
+ActiveRecord::Schema.define(version: 2021_04_09_225633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorite_movies", force: :cascade do |t|
+    t.string "description"
+    t.bigint "movie_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["movie_id"], name: "index_favorite_movies_on_movie_id"
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -25,4 +33,5 @@ ActiveRecord::Schema.define(version: 2021_03_30_000618) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "favorite_movies", "movies"
 end
